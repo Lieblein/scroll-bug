a996e4cd9d57f37a658be1cc7efa07c8bce9fff8
Ложкина Любовь <llozhkina@htc-cs.ru>
2023-12-21 15:19:18 +0400

    add react

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './components/App';
+
+const container = document.getElementById('react-app');
+if (container !== null) {
+    const root = createRoot(container);
+    root.render(<App />);
+}
